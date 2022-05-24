95262dd0dc714d5cd27b445c6eeb27a13f429b0f
MarkusMark1 <mark.ralstondaniel@mail.mcgill.ca>
2022-05-23 21:29:39 -0400

    Updated to React18  Root settings

--- src/App.js
+++ src/App.js
@@ -1,6 +1,29 @@
+
+
 function App () {
+
+    const comments = [
+        {id: 3, text: 'Three'},
+        {id: 2, text: 'Two'},
+        {id: 1, text: 'One'},
+
+    ]
+
+    const showComments = true;
+    const commentBlocks= ( <ul>
+        {comments.map((comment,index) => (
+            <li key={index}> {comment.text} </li>
+        ))}
+        </ul>)
+
+    
     return (
+        <div className="div"> 
         <h1>Hello from the app component</h1>
+
+        {showComments && commentBlocks }
+
+        </div>
     )
 }
 
