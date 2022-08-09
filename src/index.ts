2ad7b466ff25465b92764f2e567e0fbd9e3c5f8e
Mark Daniel <83738282+Mark-777-0@users.noreply.github.com>
2022-08-09 19:52:08 -0400

    Converting to ts

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import {createRoot} from 'react-dom/client'
 import './index.css'
 import App from './App'
 
+//adding typescript
 const container = document.getElementById('root')
 
 const root = createRoot(container);
